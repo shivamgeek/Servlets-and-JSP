58c7d1cce85a505b90e7e673298c5323b6521ecf
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-09 16:46:19 +0530

    Changed to Login, added RequestDispatcher

--- src/main/java/com/shivam/servlets/loginServlet.java
+++ src/main/java/com/shivam/servlets/loginServlet.java
@@ -3,29 +3,41 @@ package com.shivam.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class addServlet extends HttpServlet {
+public class loginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public addServlet() {
+    public loginServlet() {
         super();
     }
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		int num1 = Integer.parseInt(request.getParameter("num1"));
-		int num2 = Integer.parseInt(request.getParameter("num2"));
-		int sum = num1 + num2;
+		String user = request.getParameter("username");
+		String pass = request.getParameter("password");
 		PrintWriter out = response.getWriter();
-		out.println("SUM IS "+sum);
+		out.println("doGET invoked!!<br>");
+		
+		if(user.equals("s")) {
+			RequestDispatcher rd = request.getRequestDispatcher("profilePage");
+			rd.forward(request, response);
+		}else {
+			RequestDispatcher rd = request.getRequestDispatcher("takeInput.html");
+			rd.forward(request, response);
+		}
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("doPOST invoked!!<br>");
 		doGet(request, response);
 	}
 
